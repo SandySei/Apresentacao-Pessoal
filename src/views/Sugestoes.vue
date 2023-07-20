e279d3750abd22d884ae431ca2a4aa3eb29ce8b5
Sandy <sandyalves8@hotmail.com>
2023-07-20 11:31:59 -0300

    Pequenas alterações de layout

--- src/views/Sugestoes.vue
+++ src/views/Sugestoes.vue
@@ -1,186 +1,196 @@
 <template>
-    <body>
-<h2>Sugestões:</h2>
+  <body>
+    <h2>Sugestões:</h2>
 
+    <div id="livro1">
+      <br />
+      <h3>Livro: O bom Filho</h3>
 
-<div id="livro1">  
-    <br>
-    <h3>Livro: O bom Filho</h3>
+      <br />
 
-<br>
-   
-    <div id="livro2">
-         <img src="/arquivos/ObomFilho.jpg" alt="Capa o Bom Filho" height="300" string="left">
-        <p>O bom filho, fenômeno literário que rendeu a You-jeong Jeong o apelido de "Stephen King coreana", é um thriller psicológico e um mergulho no que há de mais sombrio na alma humana. 
-            A cada resposta abrem-se mais perguntas. Neste romance perturbador e surpreendente o terror se esconde onde menos se espera.
-        <br><br>
-            Segue Resumo segundo 
-            <a href="https://books.google.com.br/books/about/O_bom_filho.html?id=pRCeDwAAQBAJ&source=kp_book_description&redir_esc=y" target="_blank">Google Books: </a>
-        <br><br>
-            Jovem com um futuro brilhante, Yu-jin vê sua carreira ser interrompida pela epilepsia.
-            Os remédios que previnem seus ataques acabam por cobrar um preço alto, e o sonho de ser um esportista é sepultado para sempre.
-            Isso não o impede de sair escondido todas as noites para correr, contrariando sua mãe. 
-            Para ele, os riscos à saúde não se comparam aos prazeres da velocidade e da força. 
-            Numa manhã qualquer, Yu-jin desperta sentindo cheiro de sangue. 
-            Tudo indica que tenha sofrido um ataque epiléptico à noite, mas, ao percorrer o apartamento, encontra o corpo da mãe ao pé da escada. 
-            Aos poucos, sua memória vai voltando, e ele tem a lembrança de tê-la ouvido chamar seu nome. 
-            Não está certo, no entanto, se ela pedia ajuda ou se tentava salvar a própria vida. 
-            Começa assim a busca desesperada do protagonista para esclarecer o que ocorreu na noite anterior. 
-            Juntando algumas poucas pistas, Yu-jin tentará montar o quebra-cabeça e descobrir o assassino. 
-            Conforme prossegue na investigação, procurará na própria memória as explicações para o crime, mas o passado esconde armadilhas mais tenebrosas do que ele pode prever. 
-        <br><br>
+      <div id="livro2">
+        <img
+          src="/arquivos/ObomFilho.jpg"
+          alt="Capa o Bom Filho"
+          height="300"
+          string="left"
+        />
+        <p>
+          O bom filho, fenômeno literário que rendeu a You-jeong Jeong o apelido
+          de "Stephen King coreana", é um thriller psicológico e um mergulho no
+          que há de mais sombrio na alma humana. A cada resposta abrem-se mais
+          perguntas. Neste romance perturbador e surpreendente o terror se
+          esconde onde menos se espera. <br /><br />
+          Segue Resumo segundo
+          <a
+            href="https://books.google.com.br/books/about/O_bom_filho.html?id=pRCeDwAAQBAJ&source=kp_book_description&redir_esc=y"
+            target="_blank"
+            >Google Books:
+          </a>
+          <br /><br />
+          Jovem com um futuro brilhante, Yu-jin vê sua carreira ser interrompida
+          pela epilepsia. Os remédios que previnem seus ataques acabam por
+          cobrar um preço alto, e o sonho de ser um esportista é sepultado para
+          sempre. Isso não o impede de sair escondido todas as noites para
+          correr, contrariando sua mãe. Para ele, os riscos à saúde não se
+          comparam aos prazeres da velocidade e da força. Numa manhã qualquer,
+          Yu-jin desperta sentindo cheiro de sangue. Tudo indica que tenha
+          sofrido um ataque epiléptico à noite, mas, ao percorrer o apartamento,
+          encontra o corpo da mãe ao pé da escada. Aos poucos, sua memória vai
+          voltando, e ele tem a lembrança de tê-la ouvido chamar seu nome. Não
+          está certo, no entanto, se ela pedia ajuda ou se tentava salvar a
+          própria vida. Começa assim a busca desesperada do protagonista para
+          esclarecer o que ocorreu na noite anterior. Juntando algumas poucas
+          pistas, Yu-jin tentará montar o quebra-cabeça e descobrir o assassino.
+          Conforme prossegue na investigação, procurará na própria memória as
+          explicações para o crime, mas o passado esconde armadilhas mais
+          tenebrosas do que ele pode prever.
+          <br /><br />
         </p>
+      </div>
+    </div>
 
-    </div>    
-</div> 
-
-<br>
-<br>
+    <br />
+    <br />
 
     <h3>Musicas: Rap Italiano</h3>
 
-<br>
+    <br />
 
-    <iframe style="border-radius:0%" src="https://open.spotify.com/embed/artist/7BgEOZ9w3Y4IMShXTMu1nN?utm_source=generator" 
-        frameBorder="0" 
-        allowtransparency="true"
-        allowfullscreen="false" 
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture; transparency" 
-        loading="lazy">
-    </iframe>
+    <iframe
+      style="border-radius: 12px"
+      src="https://open.spotify.com/embed/artist/7BgEOZ9w3Y4IMShXTMu1nN?utm_source=generator&theme=0"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allowfullscreen="false"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
 
-    
+    <br />
+    <br />
+    <br />
+    <div id="Poemas1">
+      <br />
+      <h3>Poema: Amar de Carlos Drummond de Andrade</h3>
 
-<br>
-<br>
-<br>
-<div id="Poemas1">
-    <br>
-    <h3>Poema: Amar de Carlos Drummond de Andrade </h3>  
-   
-<br>
+      <br />
 
-    <div id="Poemas2">
+      <div id="Poemas2">
         <p>
-            Que pode uma criatura senão,<br>
-            entre criaturas, amar?<br>
-            amar e esquecer, amar e malamar,<br>
-            amar, desamar, amar?<br>
-            sempre, e até de olhos vidrados, amar?<br>
-            <br>
-            Que pode, pergunto, o ser amoroso,<br>
-            sozinho, em rotação universal,<br>
-            senão rodar também, e amar?<br>
-            amar o que o mar traz à praia,<br>
-            o que ele sepulta, e o que, na brisa marinha,<br>
-            é sal, ou precisão de amor, ou simples ânsia?<br>
-            <br>
-            Amar solenemente as palmas do deserto,<br>
-            o que é entrega ou adoração expectante,<br>
-            e amar o inóspito, o cru,<br>
-            um vaso sem flor, um chão de ferro,<br>
-            e o peito inerte, e a rua vista em sonho, e<br>
-            uma ave de rapina.<br>
-            <br>
-            Este o nosso destino: amor sem conta,<br>
-            distribuído pelas coisas pérfidas ou nulas,<br>
-            doação ilimitada a uma completa ingratidão,<br>
-            e na concha vazia do amor a procura medrosa,<br>
-            paciente, de mais e mais amor.<br>
-            <br>
-            Amar a nossa falta mesma de amor,<br>
-            e na secura nossa amar a água implícita,<br>
-            e o beijo tácito, e a sede infinita.<br>
-            <br><br>
+          Que pode uma criatura senão,<br />
+          entre criaturas, amar?<br />
+          amar e esquecer, amar e malamar,<br />
+          amar, desamar, amar?<br />
+          sempre, e até de olhos vidrados, amar?<br />
+          <br />
+          Que pode, pergunto, o ser amoroso,<br />
+          sozinho, em rotação universal,<br />
+          senão rodar também, e amar?<br />
+          amar o que o mar traz à praia,<br />
+          o que ele sepulta, e o que, na brisa marinha,<br />
+          é sal, ou precisão de amor, ou simples ânsia?<br />
+          <br />
+          Amar solenemente as palmas do deserto,<br />
+          o que é entrega ou adoração expectante,<br />
+          e amar o inóspito, o cru,<br />
+          um vaso sem flor, um chão de ferro,<br />
+          e o peito inerte, e a rua vista em sonho, e<br />
+          uma ave de rapina.<br />
+          <br />
+          Este o nosso destino: amor sem conta,<br />
+          distribuído pelas coisas pérfidas ou nulas,<br />
+          doação ilimitada a uma completa ingratidão,<br />
+          e na concha vazia do amor a procura medrosa,<br />
+          paciente, de mais e mais amor.<br />
+          <br />
+          Amar a nossa falta mesma de amor,<br />
+          e na secura nossa amar a água implícita,<br />
+          e o beijo tácito, e a sede infinita.<br />
+          <br /><br />
         </p>
-
+      </div>
     </div>
-</div>
-<br><br><br> 
-
-</body>
+    <br /><br /><br />
+  </body>
 </template>
 
 <style scoped>
-
 body {
-    background-image: url(/arquivos/fundoconcreto.jpg);
-    background-size: cover;
-    min-height: 140vh;
-    background-attachment: fixed;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center ;
+  background-image: url(/arquivos/fundoconcreto.jpg);
+  background-size: cover;
+  min-height: 140vh;
+  background-attachment: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 iframe {
-    display: flex;
-    flex-direction: column;
-    height: 65vh;
-    width: 142vh;
+  display: flex;
+  flex-direction: column;
+  height: 65vh;
+  width: 142vh;
 }
 
-
-
 h2 {
-    color: #000000;
-    font-family:Arial;
-    font-size:50px;
-    text-transform: uppercase;
-    font-weight:bold;
-    text-align: center;
-    line-height: 2.5;
+  color: #000000;
+  font-family: Arial;
+  font-size: 50px;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-align: center;
+  line-height: 2.5;
 }
 
 h3 {
-    color: #000000;
-    font-family:Arial;
-    font-size:25px;
-    text-transform: uppercase;
-    font-weight:bold;
-    text-align: center;
-    line-height: 2;
+  color: #000000;
+  font-family: Arial;
+  font-size: 25px;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-align: center;
+  line-height: 2;
 }
 
 h4 {
-    color: #000000;
-    font-family:Arial;
-    font-size:25px;
-    text-align: center;
-    line-height: 1;
+  color: #000000;
+  font-family: Arial;
+  font-size: 25px;
+  text-align: center;
+  line-height: 1;
 }
 
 img {
-    border-radius: 230px;
-    border: 4px solid #bdbdbd;
-    display: block;
-    text-align: center;
-
+  border-radius: 230px;
+  border: 4px solid #bdbdbd;
+  display: block;
+  text-align: center;
 }
 
 div#livro1 {
-    background-color: #70707083;
-    width: 142vh;
-    height: 52vh;
-    justify-content: center;
-    align-items: center;
-    margin-top: 5vh;
-    border: 3px solid #c4c4c486;
-    border-radius: 10px;
-  
+  background-color: #70707083;
+  width: 142vh;
+  height: 52vh;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5vh;
+  border: 3px solid #c4c4c486;
+  border-radius: 10px;
 }
 
 p {
-    text-align: justify;
-    font-family:Arial;
-    font-size:16px;
+  text-align: justify;
+  font-family: Arial;
+  font-size: 16px;
 }
 
 a {
-    color: #004fd6;
-    font-family:Arial;
-    font-size:15px;
+  color: #004fd6;
+  font-family: Arial;
+  font-size: 15px;
 }
 
 #livro2 img {
@@ -188,30 +198,29 @@ a {
 }
 
 div#livro2 {
-    display: inline-block;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    margin-left: 5vw;
-    margin-right: 5vw; 
+  display: inline-block;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-left: 5vw;
+  margin-right: 5vw;
 }
 
 div#Poemas1 {
-    background-color: #70707083;
-    width: 142vh;
-    height: 70vh;
-    justify-content: center;
-    align-items: center;
-    margin-top: 5vh;
-    border: 3px solid #c4c4c486;
-    border-radius: 10px;
+  background-color: #70707083;
+  width: 142vh;
+  height: 70vh;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5vh;
+  border: 3px solid #c4c4c486;
+  border-radius: 10px;
 }
 
-div#Poemas2 > p  {
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    text-align: center;
+div#Poemas2 > p {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  text-align: center;
 }
-
 </style>
